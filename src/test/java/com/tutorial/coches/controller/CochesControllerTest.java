55b162a7b6026d27eec1f66db1b94fae9ae7e969
Tiago <tjoelsm@gmail.com>
2021-03-05 21:06:25 +0100

    fix

--- src/test/java/com/tutorial/coches/controller/CochesControllerTest.java
+++ src/test/java/com/tutorial/coches/controller/CochesControllerTest.java
@@ -4,9 +4,9 @@
 package com.tutorial.coches.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.ArrayList;
